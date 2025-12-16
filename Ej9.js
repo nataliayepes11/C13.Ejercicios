a474ab8cde79a5954ce6722eff4c903c54ed16a6
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-16 17:33:46 -0300

    FUNCIÓN DECLARADA, CICLO FOR

--- Ej9.js
+++ Ej9.js
@@ -3,4 +3,10 @@ Crea una función declarada llamada contarHasta que reciba un número y
 use un bucle para imprimir todos los números desde 1 hasta ese número.
 */
 
-const prompt = require('prompt-sync')();
+function contarHasta(numero) {
+  for (let i = 1; i <= numero; i++) {
+    console.log(i);
+  }
+}
+
+contarHasta(11);
