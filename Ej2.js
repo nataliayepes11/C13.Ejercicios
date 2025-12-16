cd4f7884c5b700a72d9566d5c52fe48e30feb793
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-16 16:13:19 -0300

    FUNCIÓN EXPRESADA

--- Ej2.js
+++ Ej2.js
@@ -2,4 +2,13 @@
 Crea una función expresada llamada encontrarMayor que reciba dos números como parámetros y devuelva el número mayor.
 */
 
-const prompt = require('prompt-sync')();
+const encontrarMayor = function(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+};
+
+let resultado = encontrarMayor(7, 12);
+console.log("El número mayor es: " + resultado);
